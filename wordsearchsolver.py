8476b83cab6789a81ff6c1e99c92e61eacbd60cc
TheRealFanjin <fanjin627@gmail.com>
2020-06-05 16:58:36 -0500

    checkHorizontal() works but returns coordinates of last letter when finding a reversed word

--- wordsearchsolver.py
+++ wordsearchsolver.py
@@ -1,13 +1,28 @@
-print('Type the letters here, pressing enter after each row. Type . (period) when done')
+print('Type the letters here, pressing enter after each row. Press enter when done:')
+listOfLetters = []
+indexPos = []
+i = 0
 while True:
     a = input()
-    if a == '.':
+    if a == '':
         break
-listOfLetters = [[None] * width] * height
-def checkHorizontal(self):
-    for row in listOfLetters:
-       try:
-            indexPos = listOfLetters[row].index(str(word)).rindex(str(word))
-       except:
-           continue
-       return indexPos
+    listOfLetters.append(a)
+    print(listOfLetters)
+    i +=1
+def checkHorizontal():
+    indexPos = [None]*2
+    for counter, row in enumerate(listOfLetters):
+        try:
+            try:
+                indexPos[0] = row.index(word)
+                indexPos[1] = counter
+            except ValueError:
+                indexPos[0] = row.index(word[::-1])
+                indexPos[1] = counter
+        except ValueError:
+            continue
+    return indexPos
+
+while True:
+    word = input('Type the word you want to find: ')
+    print(checkHorizontal())
