133a89c92600f9e8badfa0657608fd32c3b657e4
TheRealFanjin <fanjin627@gmail.com>
2020-06-05 15:12:20 -0500

    Got the inputting letters to work

---
+++ wordsearchsolver.py
@@ -0,0 +1,13 @@
+print('Type the letters here, pressing enter after each row. Type . (period) when done')
+while True:
+    a = input()
+    if a == '.':
+        break
+listOfLetters = [[None] * width] * height
+def checkHorizontal(self):
+    for row in listOfLetters:
+       try:
+            indexPos = listOfLetters[row].index(str(word)).rindex(str(word))
+       except:
+           continue
+       return indexPos
